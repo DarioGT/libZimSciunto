dd1153116753fcf9354436df54cc6c1c0c48cb7e
François Boulogne <fboulogne@sciunto.org>
2012-09-10 20:03:12 +0200

    first release

--- libzimsciunto/info.py
+++ libzimsciunto/info.py
@@ -8,7 +8,7 @@ NAME = "libZimSciunto"
 URL = "https://github.com/sciunto/libZimSciunto"
 LICENSE = "GPLv3+"
 EMAIL = "fboulogne@sciunto.org"
-VERSION = "dev"
+VERSION = "0.1"
 
 SHORT_DESCRIPTION = "Library for sciunto's zim tools"
 
