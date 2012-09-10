f53309a843fe8d928eeaecdb4929d899c0bd4466
François Boulogne <fboulogne@sciunto.org>
2012-09-10 19:27:49 +0200

    add license

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     version      = info.VERSION,
     url          = info.URL,
     author       = "Francois Boulogne",
+    license      = 'LGPLv3',
     author_email = info.EMAIL,
     description  = info.SHORT_DESCRIPTION,
     packages = ['libzimsciunto'],
