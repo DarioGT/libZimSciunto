ac7d263c730153dc8d0eff999c49b89980aa8162
François Boulogne <fboulogne sciunto org>
2012-09-30 16:04:09 +0200

    release 0.2

--- libzimsciunto/info.py
+++ libzimsciunto/info.py
@@ -8,7 +8,7 @@ NAME = "libZimSciunto"
 URL = "https://github.com/sciunto/libZimSciunto"
 LICENSE = "LGPLv3+"
 EMAIL = "fboulogne@sciunto.org"
-VERSION = "0.1"
+VERSION = "0.2"
 
 SHORT_DESCRIPTION = "Library for sciunto's zim tools"
 
